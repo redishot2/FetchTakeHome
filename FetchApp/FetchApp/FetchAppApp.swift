c77a914adb92dec645609f253f358d432d1c44eb
Natasha Martinez <nem4265@g.rit.edu>
2025-02-24 17:56:00 -0500

    First pass add

---
+++ FetchApp/FetchApp/FetchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchAppApp.swift
+//  FetchApp
+//
+//  Created by Natasha Martinez on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
